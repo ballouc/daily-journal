746978886a656eb9168318685b890b06496f6b92
ballouc <cmbalp@gmail.com>
2024-02-27 21:49:07 -0500

    vercel update

--- src/components/Entry.tsx
+++ src/components/Entry.tsx
@@ -1,30 +1,30 @@
-import React, { useState } from 'react'
-import Input from './Input';
+// import React, { useState } from 'react'
+// import Input from './Input';
 
-interface Props{
-    title: string;
-}
+// interface Props{
+//     title: string;
+// }
 
-interface Input{
-    title: string;
-    type: string;
-    value: string;
-}
+// interface Input{
+//     title: string;
+//     type: string;
+//     value: string;
+// }
 
-const Entry: React.FC<Props> = ({ title }) => {
+// const Entry: React.FC<Props> = ({ title }) => {
 
-    const [entryInputs, setEntryInputs] = useState<Input[]>([{title: 'Daily Gratitude', type: 'text-sm', value: 'Lorem'}, {title: 'Notes', type: 'text-lg', value: 'Ipsum'}]);
+//     const [entryInputs, setEntryInputs] = useState<Input[]>([{title: 'Daily Gratitude', type: 'text-sm', value: 'Lorem'}, {title: 'Notes', type: 'text-lg', value: 'Ipsum'}]);
 
-  return (
-    <>
-        <div className="bold">{title}</div>
-        {entryInputs.map((input, index) => {
-            return(
-                <div className={input.type}>{input.value}</div>
-            )
-        })}
-    </>
-  )
-}
+//   return (
+//     <>
+//         <div className="bold">{title}</div>
+//         {entryInputs.map((input, index) => {
+//             return(
+//                 <div className={input.type}>{input.value}</div>
+//             )
+//         })}
+//     </>
+//   )
+// }
 
-export default Entry
+// export default Entry
